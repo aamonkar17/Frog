a5cb804e123369df662dc68400a930b02db12d20
Ankit Amonkar <ankit.amonkar@gmail.com>
2019-10-09 10:38:09 -0400

    done fraction

--- src/com/company/Fraction.java
+++ src/com/company/Fraction.java
@@ -1,9 +1,7 @@
 package com.company;
 
-public class Main {
+public class Fraction {
 
-    public static void main(String[] args) {
-        public class Fraction {
             private int numerator;		//attributes
             private int denominator;
 
@@ -41,44 +39,50 @@ public class Main {
             public Fraction subtract(Fraction other){
                 int n = this.numerator*other.denominator - this.denominator*other.numerator;
                 int d = this.denominator * other.getDenom();
+                Fraction result = new Fraction(n,d);
+                return result;
             }
 
 
             // TODO multiply()
             public Fraction multiply(Fraction other){
-                int n = this.numerator*other.denominator * this.denominator*other.numerator;
+                int n = this.numerator*other.numerator;
                 int d = this.denominator * other.getDenom();
+                Fraction result = new Fraction(n,d);
+                return result;
             }
 
             // TODO toString()
 
             public String toString() {
-                System.out.println(numerator + "/" + denominator);
+                int n = numerator;
+                int d = denominator;
+                String s = (n + "/" + d);
+                return s;
             }
 
             public static void main(String[] args) {
                 Fraction f1 = new Fraction(3,4);	// create an instance of fraction
                 Fraction f2 = new Fraction(4,5);
+                Fraction f6 = new Fraction(2,3);
+                Fraction f7 = new Fraction(4,7);
+
 
                 Fraction f3 = f1.add(f2);			// add 2 fractions
                 System.out.println("3/4 + 4/5 = " + f3);	// print the answer
 
-                Fraction f4 = f1.multiply(f2);
-                System.out.println(f4);
 
-		/*
-		Fraction f4 = f1.subtract(f2);
-		System.out.println(f1 + " - " + f2 + " = " + f4);
+		        Fraction f4 = f1.subtract(f2);
+		        System.out.println(f1 + " - " + f2 + " = " + f4);
 
-		Add code to call multiply f1 and f2 here
+                Fraction f5 = f1.multiply(f2);
+                System.out.println(f1 + " - " + f2 + " = " + f5);
 
-		create 2 new fractions and add them here.
-
-		*/
+                Fraction f8 = f6.add(f7);			// add 2 fractions
+                System.out.println("21/25 + 3/25 = " + f8);
 
 
             }
 
         }
-    }
-}
+
