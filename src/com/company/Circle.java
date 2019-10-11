5f6733669e52c0cfe09299f97a5020805f13f20b
Ankit Amonkar <ankit.amonkar@gmail.com>
2019-10-11 12:30:19 -0400

    Line

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,5 +1,23 @@
 package com.company;
 
 public class Circle {
+    private double radius;
+    private double c1;
+    private double c2;
+    private double a;
 
+    public Circle(double radius){
+
+        c1 = Circle(r1);
+        c2 = Circle(r2);
+    }
+
+    public double area(){
+        a = Math.PI * Math.pow(radius, 2);
+        return a;
+    }
+
+    public double circumference(){
+        c = 
+    }
 }
