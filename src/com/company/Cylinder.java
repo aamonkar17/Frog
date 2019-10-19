f075b21bd5c48ad1d0cbacab201739bc462fb7ba
Ankit Amonkar <ankit.amonkar@gmail.com>
2019-10-19 14:51:49 -0400

    cylinder

--- src/com/company/Cylinder.java
+++ src/com/company/Cylinder.java
@@ -1,16 +1,16 @@
 package com.company;
 
-public class CylinderTest {
+public class Cylinder {
     private double base;
     private double height;
     private double h;
     private double r;
+    private Circle c1;
 
-    public CylinderTest(double height, double radius) {
+    public Cylinder(double height, double radius) {
         h = height;
         r = radius;
-        double c1 = new Circle(r);
-        c1 *= h;
+        c1 = new Circle(r);
     }
 
     public double vol() {
@@ -19,23 +19,7 @@ public class CylinderTest {
     }
 
     public double sa() {
-        double s = 2 * Math.PI * r * h + 2 * Math.PI * Math.pow (r, 2);
+        double s = (2 * Math.PI * r * h) + (2 * Math.PI * Math.pow(r, 2));
         return s;
-    }
-
-    public static void main(String[] args) {
-
-        Cylinder cyl = new Cylinder(5,2);
-            double vol = cyl.vol();
-            System.out.println("radius = 5 height = 2 volume = " + vol);
-            double sa = cyl.sa();
-            System.out.println("surface area = " + sa);
-
-            Cylinder cyl2 = new Cylinder(8,6);
-            vol = cyl2.vol();
-            System.out.println("radius = 8 height = 6 volume = " + vol);
-            sa = cyl2.sa();
-            System.out.println("surface area = " + sa);
-        }
     }
 }
